5b65ce18de88db883a4951ba35e8878cffccc567
rkpatel94 <rajkumar.patel1994@gmail.com>
2023-07-18 15:28:54 +0530

    add graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Graph from "./Graph";
 
-function App() {
+import "./style.css";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  height: "100%",
+  width: "100%"
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+      <Graph
+        width="100%"
+        nodes={[
+          { id: 1, name: "node 1", dependsOn: [] },
+          { id: 7, name: "node 7", dependsOn: [] },
+          { id: 2, name: "node 2", dependsOn: [1] },
+          { id: 3, name: "node 3", dependsOn: [2] },
+          { id: 4, name: "node 4", dependsOn: [2] },
+          { id: 5, name: "node 5", dependsOn: [4, 7] },
+          { id: 6, name: "node 6", dependsOn: [5] }
+        ]}
+      />
     </div>
   );
-}
+};
 
-export default App;
+export default App
